a81071821d072448704cde301af9a21d468f4a53
ihaai <69941441+ihaai@users.noreply.github.com>
2022-07-03 09:33:04 +0000

    Added class for Regex
    This class will contain any regex that I use.

---
+++ Scraper/Scraper/Expressions/Regexr.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Scraper.Expressions
+{
+    internal class Regexr
+    {
+        public static Regex ThreadLinkRegex = new Regex(@"boards\.(4chan|4channel)\.org\/(.*?)\/thread\/(\d+)");
+
+        public static Regex ImageLinkRegex = new Regex("<a\\s+class=\"fileThumb\"\\s+href=\"\\/\\/(.*?)\"");
+
+        public static Regex FileNameRegex = new Regex(@"(.*)\/(.*)");
+    }
+}
