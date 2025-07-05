622c458c5cb39d562b1b3f88e7f1487136ec19d9
nil-lambda <69941441+nil-lambda@users.noreply.github.com>
2025-07-05 23:56:13 +0300

    Configured HttpClient for compatibility
    HttpClient now uses predefined headers and automatic decompression. The headers include User-Agent and Cookie from the configuration file, along with additional headers to make sure the request goes through successfully. Some code has been refactored.

--- Scraper/Scraper/Menu.cs
+++ Scraper/Scraper/Menu.cs
@@ -8,15 +8,21 @@ namespace Scraper
     {
         public Menu() => InitializeComponent();
 
-        private string? DirectoryName { get; set; }
+        private string? DirectoryName;
 
-        private string? BoardTag { get; set; }
+        private string? BoardTag;
 
-        private string? BoardId { get; set; }
+        private string? BoardId;
 
-        private int SuccessCount { get; set; }
+        private int SuccessCount;
+
+        private bool CanScrape;
 
         private Dictionary<string, string> RequestConfig;
+
+        private HttpClientHandler ClientHandler;
+
+        private HttpClient Client;
 
         private void Menu_Load(object sender, EventArgs e)
         {
@@ -25,7 +31,7 @@ namespace Scraper
 
             ReadAndAssignConfig();
         }
-        
+
         private void ReadAndAssignConfig()
         {
             const string configFileName = "config.env";
@@ -33,48 +39,56 @@ namespace Scraper
 
             if (!File.Exists(configFileName))
             {
-                using (StreamWriter stream = new(File.Create(configFileName)))
-                {
-                    stream.WriteLine("USER_AGENT=");
-                    stream.WriteLine("COOKIE=");
-                }
+                File.WriteAllLines(configFileName, new[] { "USER_AGENT=", "COOKIE=" });
 
                 MessageBox.Show("File \"config.env\" was not found, so it has been created. Add information in it to avoid errors. The scraper will now exit.", "Config error");
                 Environment.Exit(0);
             }
 
-            using (StreamReader stream = new StreamReader(configFileName))
+            foreach (string currentLine in File.ReadAllLines(configFileName))
             {
-                while (!stream.EndOfStream)
+                string[] currentRequestHeader = currentLine.Split('=', 2);
+
+                if (string.IsNullOrEmpty(currentRequestHeader[1]))
                 {
-                    string currentLine = stream.ReadLine();
-                    string[] currentRequestHeader = currentLine.Split('=', 2);
+                    MessageBox.Show($"{currentRequestHeader[0]} has no given value in \"config.env\". You wont be able to scrape.", "Config error");
+                    this.CanScrape = false;
 
-                    if (string.IsNullOrEmpty(currentRequestHeader[1]))
-                    {
-                        MessageBox.Show($"{currentRequestHeader[0]} has no given value in \"config.env\". You wont be able to scrape.", "Config error");
-                        return;
-                    }
-
-                    this.RequestConfig[currentRequestHeader[0].Trim()] = currentRequestHeader[1].Trim();
-
-                    /*
-                     * currentRequestHeader[0] is the request header (USER_AGENT)
-                     * currentRequestHeader[1] is the request header value given by the user (Mozilla/5.0...)
-                     */
+                    return;
                 }
+
+                this.RequestConfig[currentRequestHeader[0].Trim()] = currentRequestHeader[1].Trim();
+
+                /*
+                 * currentRequestHeader[0] is the request header (USER_AGENT)
+                 * currentRequestHeader[1] is the request header value given by the user (Mozilla/5.0...)
+                 */
             }
+
+            this.ClientHandler = new HttpClientHandler()
+            {
+                AutomaticDecompression = System.Net.DecompressionMethods.All
+            };
+
+            /* Additional predefined headers to make sure the request goes through successfully */
+            this.Client = new HttpClient(this.ClientHandler);
+            this.Client.DefaultRequestHeaders.Add("User-Agent", this.RequestConfig["USER_AGENT"]);
+            this.Client.DefaultRequestHeaders.Add("Cookie", this.RequestConfig["COOKIE"]);
+            this.Client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8");
+            this.Client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.5");
+            this.Client.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
 
             configLabel.Text = "[Cookie && Useragent set]";
             configLabel.ForeColor = Color.Green;
+            this.CanScrape = true;
         }
 
         private void CreateDirectory()
         {
-            if (Directory.Exists($"C:\\Scraper\\{DirectoryName}"))
+            if (Directory.Exists($"C:\\Scraper\\{this.DirectoryName}"))
                 return;
 
-            Directory.CreateDirectory($"C:\\Scraper\\{DirectoryName}");
+            Directory.CreateDirectory($"C:\\Scraper\\{this.DirectoryName}");
         }
 
         private async void ScrapeButton_Click(object sender, EventArgs e)
@@ -83,33 +97,31 @@ namespace Scraper
 
             listBox1.Items.Clear();
 
-            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage statusCode = await this.Client.GetAsync(linkBox.Text))
             {
-                using (HttpResponseMessage statusCode = await client.GetAsync(linkBox.Text))
+                pageStatusLabel.Text = $"[Page returned {statusCode.StatusCode}]";
+
+                switch (statusCode.IsSuccessStatusCode)
                 {
-                    pageStatusLabel.Text = $"[Page returned {statusCode.StatusCode}]";
-
-                    switch (statusCode.IsSuccessStatusCode)
-                    {
-                        case true: pageStatusLabel.ForeColor = Color.Green; break;
-                        case false: pageStatusLabel.ForeColor = Color.Red; return;
-                    }
-
-                    Task<string> urlContent = client.GetStringAsync(linkBox.Text);
-
-                    foreach (Match imageMatch in Regexr.ImageLinkRegex.Matches(urlContent.Result))
-                    {
-                        listBox1.Items.Add(imageMatch.Groups[1].Value);
-                    }
-
-                    scrapeTotalLabel.ForeColor = Color.DarkMagenta;
-                    scrapeTotalLabel.Text = $"[Trying to scrape {listBox1.Items.Count} items...]";
-
-                    await Download();
-
-                    scrapeTotalLabel.ForeColor = Color.Green;
-                    scrapeTotalLabel.Text = $"[Scraped {SuccessCount} items]";
+                    case true: pageStatusLabel.ForeColor = Color.Green; break;
+                    case false: pageStatusLabel.ForeColor = Color.Red; return;
                 }
+
+                Task<string> threadHtmlContent = this.Client.GetStringAsync(linkBox.Text);
+
+                foreach (Match imageMatch in Regexr.ImageLinkRegex.Matches(threadHtmlContent.Result))
+                {
+                    listBox1.Items.Add(imageMatch.Groups[1].Value);
+                }
+
+                scrapeTotalLabel.ForeColor = Color.DarkMagenta;
+                scrapeTotalLabel.Text = $"[Trying to scrape {listBox1.Items.Count} items...]";
+
+                await Download();
+
+                scrapeTotalLabel.ForeColor = Color.Green;
+                scrapeTotalLabel.Text = $"[Scraped {this.SuccessCount} items]";
+                this.SuccessCount = 0;
             }
         }
 
@@ -121,15 +133,12 @@ namespace Scraper
                 {
                     string fileName = getName.Groups[2].Value;
 
-                    using (FileStream fileWriter = new FileStream($"C:\\Scraper\\{DirectoryName}\\{fileName}", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
+                    using (FileStream fileWriter = new FileStream($"C:\\Scraper\\{this.DirectoryName}\\{fileName}", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite))
                     {
-                        using (HttpClient client = new HttpClient())
+                        using (Stream imageStream = await this.Client.GetStreamAsync($"https://{item}"))
                         {
-                            using (Stream imageStream = await client.GetStreamAsync($"https://{item}"))
-                            {
-                                await imageStream.CopyToAsync(fileWriter);
-                                SuccessCount++;
-                            }
+                            await imageStream.CopyToAsync(fileWriter);
+                            this.SuccessCount++;
                         }
                     }
                 }
@@ -138,23 +147,26 @@ namespace Scraper
 
         private void LinkBox_TextChanged(object sender, EventArgs e)
         {
-            switch (Regexr.ThreadLinkRegex.IsMatch(linkBox.Text))
-            {
-                case true:
-                    scrapeButton.Enabled = true;
-                    label2.ForeColor = Color.Green;
-                    label2.Text = $"[Thread link detected - /{Regexr.ThreadLinkRegex.Match(linkBox.Text).Groups[2].Value}/]";
-                    BoardTag = Regexr.ThreadLinkRegex.Match(linkBox.Text).Groups[2].Value;
-                    BoardId = Regexr.ThreadLinkRegex.Match(linkBox.Text).Groups[3].Value;
-                    DirectoryName = $"Thread_{BoardId}_{BoardTag}";
-                    break;
+            if (!this.CanScrape) 
+                return;
 
-                case false:
-                    scrapeButton.Enabled = false;
-                    label2.ForeColor = Color.Red;
-                    label2.Text = "[Invalid link format]";
-                    break;
+            Match match = Regexr.ThreadLinkRegex.Match(linkBox.Text);
+            if (!match.Success)
+            {
+                scrapeButton.Enabled = false;
+                label2.ForeColor = Color.Red;
+                label2.Text = "[Invalid link format]";
+
+                return;
             }
+
+            scrapeButton.Enabled = true;
+            label2.ForeColor = Color.Green;
+            label2.Text = $"[Thread link detected - /{match.Groups[2].Value}/]";
+
+            this.BoardTag = match.Groups[2].Value;
+            this.BoardId = match.Groups[3].Value;
+            this.DirectoryName = $"Thread_{this.BoardId}_{this.BoardTag}";
         }
     }
 }
